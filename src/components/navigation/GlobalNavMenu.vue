8725b8b0d67a4ebfb36fd92c230c2c645a2172ac
kdieoke <kdieoke@gmail.com>
2025-03-17 19:21:37 +0100

    navbar

---
+++ src/components/navigation/GlobalNavMenu.vue
@@ -0,0 +1,32 @@
+<template>
+    <ion-menu side="end" content-id="main-content">
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Menu Content</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <ion-list lines="full">
+                <ion-item  href="/settings" v-if="!$route.path.includes('settings')">
+                    <ion-label>Einstellungen</ion-label>
+                </ion-item>
+                <ion-item  href="/home" v-if="!$route.path.includes('home')">
+                    <ion-label>Home</ion-label>
+                </ion-item>
+                <ion-item  href="/profile-page" v-if="!$route.path.includes('profile')">
+                    <ion-label>Profil</ion-label>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-menu>
+</template>
+
+<script setup>
+    import { IonMenu, menuController, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel } from '@ionic/vue';
+    import { useRouter } from 'vue-router';
+    const router = useRouter();
+    const redirect = (path = '/home') => {
+        menuController.close();
+        router.push(path);
+    }; 
+</script>
---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,97 @@
+<template>
+    <GlobalNavMenu />
+    <ion-page id="main-content">
+      <GlobalHeader />
+        <!-- Body Bereich -->
+        <ion-content>
+
+          <ion-list lines="full">
+            <!-- Item-Profile -->
+            <ion-item :button="true" router-link="/profile-page">
+              <ion-label>Profil</ion-label>
+            </ion-item>
+    
+            <!-- Item-Benachrichtigungen -->
+            <ion-item :button="true" router-link="/notification-page">
+              <ion-label>Benachrichtigungen</ion-label>
+            </ion-item>
+    
+            <!-- Item-Sprache -->
+            <ion-item :button="true" router-link="/language-page">
+              <ion-label>Sprache</ion-label>
+            </ion-item>
+    
+            <!-- Item-Design -->
+            <ion-item :button="true" router-link="/design-page">
+              <ion-label>Design</ion-label>
+            </ion-item>
+    
+            <!-- Item-Datenschutz -->
+            <ion-item :button="true" router-link="/safety-page">
+              <ion-label>Datenschutz & Sicherheit</ion-label>
+            </ion-item>
+    
+            <!-- Item-info -->
+            <ion-item :button="true" router-link="/info-page">
+              <ion-label>App Informationen</ion-label>
+            </ion-item>
+    
+            <!-- Item-Hilfe -->
+            <ion-item :button="true" router-link="/help-page">
+              <ion-label>Hilfe & Feedback</ion-label>
+            </ion-item>
+    
+            <!-- Item-Abmelden -->
+            <ion-item :button="true" @click="showActionSheet = true">
+              <ion-action-sheet
+                :is-open="showActionSheet"
+                @didDismiss="showActionSheet = false"
+                header="Bist du dir sicher?"
+                :buttons="actionButtons"
+              ></ion-action-sheet>
+              <ion-label>Abmelden</ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-content>
+      </ion-page>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { IonPage, IonContent, IonList, IonItem, IonLabel, IonActionSheet, IonHeader, IonToolbar, IonTitle } from '@ionic/vue';
+  import { GlobalHeader, GlobalNavMenu } from '../components/navigation';
+
+  const router = useRouter();
+  
+  // Action Sheet Steuerung
+  const showActionSheet = ref(false);
+  
+  // Die Buttons des Action Sheets
+  const actionButtons = [
+    {
+      text: 'Abmelden',
+      role: 'destructive',
+      handler: () => {
+        handleLogout();
+      },
+    },
+    {
+      text: 'Abbrechen',
+      role: 'cancel',
+      handler: () => {
+        console.log("Abmeldung abgebrochen.");
+      },
+    },
+  ];
+  
+  // Funktion für den Logout
+  const handleLogout = () => {
+    // JWT Token aus dem LocalStorage entfernen
+    localStorage.removeItem("jwt");
+  
+    // Weiterleitung zur Login-Seite
+    router.replace("/login-page");
+  };
+  </script>
+  
