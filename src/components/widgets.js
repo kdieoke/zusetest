668ba625e396b890378a64dd01e25b2f9b5f04e8
x6yr <cyanbranch@gmail.com>
2025-03-17 20:05:42 +0100

    mealplan

--- src/components/widgets.js
+++ src/components/widgets.js
@@ -1,6 +1,6 @@
 import Contacts from './widgets/Contacts.vue';
 import Döner from './widgets/Döner.vue';
 import Messages from './widgets/Messages.vue';
-import Test from './widgets/Test.vue';
+import Mealplan from './widgets/Mealplan.vue';
 
-export { Contacts, Döner, Messages, Test };
+export { Contacts, Döner, Messages, Mealplan };
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,8 +5,8 @@
         <ion-content>
             <ion-grid id="grid">
                 <Döner v-if="widgets.includes('Döner')" />
-                <Test v-if="widgets.includes('Test')"/>
-                <Tost v-if="widgets.includes('Tost')"/>
+                <Mealplan v-if="widgets.includes('Mealplan')"/>
+                <Contacts v-if="widgets.includes('Contacts')"/>
                 <Messages v-if="widgets.includes('Messages')"/>
             </ion-grid>
         </ion-content>
@@ -21,9 +21,10 @@
     } from '@ionic/vue';
     import { onMounted, ref} from 'vue';
 
-    import { Contacts, Döner, Messages, Test } from '../components/widgets';
+    import { Contacts, Döner, Messages, Mealplan } from '../components/widgets';
     import { GlobalHeader, GlobalNavMenu } from '../components/navigation';
     import config from '../config/config.json';
+    
 
     let widgets = ref([]);
 
